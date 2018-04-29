c08b80448f14094904377e903e04ccda79e67490
Zach Leskovec <33209886+zlesk64@users.noreply.github.com>
2018-04-28 20:51:33 -0700

    Update bleeeeend.sh

--- bleeeeend.sh
+++ bleeeeend.sh
@@ -1,5 +1,5 @@
     #!/bin/bash
-    # Ask the user for their name
+    #Run blender to render a file on the tx2
     echo Hello, Welcome to the Jetson Blender Render System currently it only supports cpu renders please type the absolute path to your file. eg. /home/user-name/file.blend.
     read varname
     blender -b $varname -x 1 -o //out-file -a
